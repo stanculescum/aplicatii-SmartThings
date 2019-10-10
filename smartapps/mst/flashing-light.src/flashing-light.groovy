4765a1bc8a47b50a78bbc289d4c888dc4fc46379
Mihail Stănculescu <stanculescum@gmail.com>
2019-10-10 16:03:46 +0300

    versiunea 1

--- smartapps/mst/flashing-light.src/flashing-light.groovy
+++ smartapps/mst/flashing-light.src/flashing-light.groovy
@@ -17,24 +17,34 @@ definition(
     description: "Flashing light in response to motion, an open/close event, or a switch.",
     category: "My Apps",
     iconUrl: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
-    iconX2Url: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png"
+    iconX2Url: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
+    iconX3Url: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png"
 )
 
 preferences {
-	section("When any of the following devices trigger..."){
-		input "motion", "capability.motionSensor", title: "Motion Sensor?", required: false
-		input "contact", "capability.contactSensor", title: "Contact Sensor?", required: false
-		input "acceleration", "capability.accelerationSensor", title: "Acceleration Sensor?", required: false
-		input "mySwitch", "capability.switch", title: "Switch?", required: false
-		input "myPresence", "capability.presenceSensor", title: "Presence Sensor?", required: false
+	section() {
+        paragraph image: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
+                  title: "Flashing light",
+                  required: true,
+                  "Flashing light in response to motion, an open/close event, or a switch."
+    }
+    section(hideWhenEmpty: true, "When any of the following devices trigger..."){
+		input "myPresence", "capability.presenceSensor", title: "Select Presence Sensors", required: false, multiple: true
+        input "motion", "capability.motionSensor", title: "Select Motion Sensors", required: false, multiple: true
+		input "contact", "capability.contactSensor", title: "Select Contact Sensors", required: false, multiple: true
+        input "mySwitch", "capability.switch", title: "SelectSwitchs", required: false, multiple: true
+        input "valves", "capability.valve", title: "Select Valves", required: false, multiple: true
+        input "smokeDetector", "capability.smokeDetector", title: "Select Smoke Detector", required: false, multiple: true
+        input "waterSensor", "capability.waterSensor", title: "Select Water Sensor", required: false, multiple: true
+        input "myLock", "capability.lock", title: "Select Lock", required: false, multiple: true
 	}
-	section("Then flash..."){
-		input "switches", "capability.switch", title: "These lights:", multiple: true
+	section("Then flashing..."){
+		input "switches", "capability.switch", title: "These lights:", required: true, multiple: true	
+	}
+	section("Number of times & Time settings in milliseconds..."){
 		input "numFlashes", "number", title: "This number of times (default 3)", required: false
-	}
-	section("Time settings in milliseconds..."){
-		input "onFor", "number", title: "On for (default 1000)", required: false
-		input "offFor", "number", title: "Off for (default 1000)", required: false
+        input "onFor", "number", title: "On for (default 1000ms)", required: false
+		input "offFor", "number", title: "Off for (default 1000ms)", required: false
 	}
 }
 
@@ -52,41 +62,30 @@ def updated() {
 }
 
 def subscribe() {
-	if (contact) {
-		subscribe(contact, "contact.open", contactOpenHandler)
-	}
-	if (acceleration) {
-		subscribe(acceleration, "acceleration.active", accelerationActiveHandler)
-	}
-	if (motion) {
-		subscribe(motion, "motion.active", motionActiveHandler)
-	}
-	if (mySwitch) {
-		subscribe(mySwitch, "switch.on", switchOnHandler)
-	}
 	if (myPresence) {
 		subscribe(myPresence, "presence", presenceHandler)
 	}
-}
-
-def motionActiveHandler(evt) {
-	log.debug "motion $evt.value"
-	flashLights()
-}
-
-def contactOpenHandler(evt) {
-	log.debug "contact $evt.value"
-	flashLights()
-}
-
-def accelerationActiveHandler(evt) {
-	log.debug "acceleration $evt.value"
-	flashLights()
-}
-
-def switchOnHandler(evt) {
-	log.debug "switch $evt.value"
-	flashLights()
+    if (motion) {
+		subscribe(motion, "motion.active", motionActiveHandler)
+	}
+    if (contact) {
+		subscribe(contact, "contact", contactHandler)
+	}
+    if (mySwitch) {
+		subscribe(mySwitch, "switch", switchHandler)
+	}
+	if (valves) {
+		subscribe(valves, "valve", valveHandler)
+	}
+    if (smokeDetector) {
+		subscribe(smokeDetector, "smoke.detected", smokeDetectedHandler)
+	}
+    if (waterSensor) {
+		subscribe(waterSensor, "water", waterHandler)
+	}
+    if (myLock) {
+		subscribe(myLock, "lock", lockHandler)
+	}
 }
 
 def presenceHandler(evt) {
@@ -97,6 +96,62 @@ def presenceHandler(evt) {
 		flashLights()
 	}
 }
+
+def motionActiveHandler(evt) {
+	log.debug "motion.active $evt.value"
+	flashLights()
+}
+
+def contactHandler(evt) {
+	log.debug "contact $evt.value"
+    if (evt.value == "open") {
+		flashLights()
+	} else if (evt.value == "closed") {
+	flashLights()
+    }
+}
+
+def switchHandler(evt) {
+	log.debug "switch $evt.value"
+	if (evt.value == "on") {
+		flashLights()
+	} else if (evt.value == "off") {
+		flashLights()
+	}
+}
+
+def valveHandler(evt) {
+	log.debug "valve $evt.value"
+	if (evt.value == "open") {
+		flashLights()
+	} else if (evt.value == "closed") {
+		flashLights()
+	}
+}
+
+def smokeDetectedHandler(evt) {
+	log.debug "smoke.detected $evt.value"
+	flashLights()
+}
+
+def waterHandler(evt) {
+	log.debug "water $evt.value"
+	if (evt.value == "wet") {
+		flashLights()
+	} else if (evt.value == "dry") {
+		flashLights()
+	}
+}
+
+def lockHandler(evt) {
+	log.debug "lock $evt.value"
+	if (evt.value == "locked") {
+		flashLights()
+	} else if (evt.value == "unlocked") {
+		flashLights()
+	}
+}
+
 
 private flashLights() {
 	def doFlash = true
