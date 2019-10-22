73787049af0cfabcc816ac62ef4c5a98f3bcce43
Stănculescu Mihail <44409999+stanculescum@users.noreply.github.com>
2019-10-22 16:15:30 +0300

    Update flashing-light.groovy

--- smartapps/mst/flashing-light.src/flashing-light.groovy
+++ smartapps/mst/flashing-light.src/flashing-light.groovy
@@ -4,7 +4,7 @@
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
@@ -22,7 +22,7 @@ definition(
 )
 
 preferences {
-	page(name: "pageOne", title: "1", nextPage: "pageTwo", uninstall: true){
+	page(name: "pageOne", title: "#1", nextPage: "pageTwo", uninstall: true){
 		section() {
         		paragraph image: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
                   	title: "Flashing light",
@@ -33,7 +33,7 @@ preferences {
 			input "switches", "capability.switch", required: true, multiple: true	
 		}
 	}
-	page(name: "pageTwo", title: "2", nextPage: "pageThree") {
+	page(name: "pageTwo", title: "#2", nextPage: "pageThree") {
 		section(hideWhenEmpty: true, "When any of the following devices trigger..."){
 			input "presence", "capability.presenceSensor", title: "Select Presence Sensors", required: false, multiple: true
 			input "motion", "capability.motionSensor", title: "Select Motion Sensors", required: false, multiple: true
@@ -45,7 +45,7 @@ preferences {
 			input "lock", "capability.lock", title: "Select Lock", required: false, multiple: true
 		}
 	}
-	page(name: "pageThree", title: "3", install: true, uninstall: true) {
+	page(name: "pageThree", title: "#3", install: true, uninstall: true) {
 		section("Start - Number of times & Time settings..."){
 			input "numStartFlashes", "number", title: "This number of times (default 1)", required: false
 			input "onStart", "number", title: "On for (default 1000ms)", required: false
