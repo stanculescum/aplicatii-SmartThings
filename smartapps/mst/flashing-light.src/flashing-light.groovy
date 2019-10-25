6e0c6a4b95689bd4dccf6da6be387a555020b772
Stănculescu Mihail <44409999+stanculescum@users.noreply.github.com>
2019-10-25 16:27:06 +0300

    Update flashing-light.groovy

--- smartapps/mst/flashing-light.src/flashing-light.groovy
+++ smartapps/mst/flashing-light.src/flashing-light.groovy
@@ -15,7 +15,7 @@ definition(
     name: "Flashing light",
     namespace: "mST",
     author: "Mihail Stanculescu",
-    description: "Flashing light in response to motion, an open/close event, an on/off switch, or lock/unlock door",
+    description: "Flashing light in response to presence/motion, an open/close event, an on/off switch, or lock/unlock door",
     category: "My Apps",
     iconUrl: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
     iconX2Url: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
