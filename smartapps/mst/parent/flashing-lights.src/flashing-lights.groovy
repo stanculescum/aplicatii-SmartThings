fcdb82442a63fdaf5c9e395b74c9480c29bcc265
Mihail Stănculescu <stanculescum@gmail.com>
2019-10-31 16:04:30 +0200

    version 1.0 / 10.2019

---
+++ smartapps/mst/parent/flashing-lights.src/flashing-lights.groovy
@@ -0,0 +1,53 @@
+/**
+ *  Flashing Lights - Parent SmartApp for SmartThings
+ *
+ *  Copyright 2019
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+definition(
+    name: "Flashing Lights",
+    namespace: "mST/parent",
+    author: "Mihail Stanculescu",
+    description: "A parent SmartApp for flashing lights.",
+    category: "My Apps",
+    iconUrl: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
+    iconX2Url: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png",
+    iconX3Url: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/flashing-light-bulb.png"
+)
+
+preferences {
+    // The parent app preferences are pretty simple: just use the app input for the child app.
+    page(name: "mainPage", title: "Simple Automations", install: true, uninstall: true,submitOnChange: true) {
+        section {
+            app(name: "simpleFlash", appName: "Flashing - child", namespace: "mST/child", title: "Create New Flashing lights", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // nothing needed here, since the child apps will handle preferences/subscriptions
+    // this just logs some messages for demo/information purposes
+    log.debug "there are ${childApps.size()} child smartapps"
+    childApps.each {child ->
+        log.debug "child app: ${child.label}"
+    }
+}
