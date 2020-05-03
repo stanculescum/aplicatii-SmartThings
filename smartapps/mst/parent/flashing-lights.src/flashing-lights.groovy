3ef4481429bab876711cba03983079bca0d71ca3
Mihail Stănculescu <stanculescum@gmail.com>
2020-05-03 17:30:32 +0300

    v1.0 / 2019-10-15 - Initial Release

--- smartapps/mst/parent/flashing-lights.src/flashing-lights.groovy
+++ smartapps/mst/parent/flashing-lights.src/flashing-lights.groovy
@@ -28,11 +28,11 @@ definition(
 preferences {
     // The parent app preferences are pretty simple: just use the app input for the child app.
     page(name: "mainPage", title: "Content", install: true, uninstall: true, submitOnChange: true) {
-        section("About") { 
-			headerSECTION()
-		}
         section {
             app(name: "Flash", appName: "Flashing-child", namespace: "mST/child", title: "Create a new automation", multiple: true, image: "https://raw.githubusercontent.com/stanculescum/aplicatii-smarthome/master/pictures/start-button.png")
+		}
+        section("About") { 
+			headerSECTION()
 		}
     }
 }
