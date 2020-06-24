6f898e5accfd8a117d295fc7df5f2b9053ab5bfa
Mihail Stănculescu <stanculescum@gmail.com>
2020-06-24 13:08:39 +0300

    Version: v1.0 / 2019-12-12 - Initial Release

--- smartapps/mst/child/zigbee-bulbs-auto-power-off-child.src/zigbee-bulbs-auto-power-off-child.groovy
+++ smartapps/mst/child/zigbee-bulbs-auto-power-off-child.src/zigbee-bulbs-auto-power-off-child.groovy
@@ -1,5 +1,5 @@
 /**
- *  ZigBee Bulbs Auto Power Off!
+ *  ZigBee Bulbs Auto Power Off!-child
  *  Copyright 2019
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
